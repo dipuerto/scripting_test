8b35888a442573c527c9cf0d53adc98b48a3a24e
dipuerto <140676310+dipuerto@users.noreply.github.com>
2023-12-05 00:49:40 -0500

    Update my_script.sh

--- my_script.sh
+++ my_script.sh
@@ -6,8 +6,8 @@ set -o pipefail
 TEST_SECRET="$1"
 
 echo "Hola desde el workflow: $TEST_SECRET"
-if [ $TEST_SECRET = "HOLA123" ]; then
-    echo "Strings are equal."
+if [ "$TEST_SECRET" = "HOLA123" ]; then
+    echo "Strings are equal v2."
 else
-    echo "Strings are not equal."
+    echo "Strings are not equal v2."
 fi
